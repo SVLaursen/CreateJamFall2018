851e453dc684d84bae2df59b70a09384fa1798d1
Simon Vestergaard Laursen <simonvlaursen@gmail.com>
2018-11-16 21:04:53 +0100

    PlayerController + Camera Controller

--- CreateFallJam2018/Assets/Scripts/Player/PlayerInput.cs
+++ CreateFallJam2018/Assets/Scripts/Player/PlayerInput.cs
@@ -5,10 +5,12 @@ public class PlayerInput : MonoBehaviour
 	[HideInInspector] public bool paused;
 	
 	private PlayerController _playerController;
+	private Camera _viewCamera;
     
 	private void Awake ()
 	{
 		_playerController = GetComponent<PlayerController>();
+		_viewCamera = FindObjectOfType<Camera>();
 	}
 	
 	private void Update ()
@@ -16,7 +18,18 @@ public class PlayerInput : MonoBehaviour
 		if (!paused)
 		{
 			//Movement
-			_playerController.Move(new Vector3(Input.GetAxisRaw("Horizontal"),0 , Input.GetAxisRaw("Vertical")));
+			_playerController.Move(new Vector3(-Input.GetAxisRaw("Vertical"), 0, Input.GetAxisRaw("Horizontal")));
+			
+			// Look input
+			Ray ray = _viewCamera.ScreenPointToRay (Input.mousePosition);
+			Plane groundPlane = new Plane (Vector3.up, Vector3.up);
+			float rayDistance;
+
+			if (groundPlane.Raycast(ray,out rayDistance)) {
+				Vector3 point = ray.GetPoint(rayDistance);
+				//Debug.DrawLine(ray.origin,point,Color.red);
+				_playerController.LookAt(point);
+			}
 		}
 	}
 }
