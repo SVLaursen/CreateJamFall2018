91a8cdd1367ab256afddcadc468ab80a9a2313cb
Simon Vestergaard Laursen <simonvlaursen@gmail.com>
2018-11-17 11:59:08 +0100

    PILLOW WALLS BABY!

--- CreateFallJam2018/Assets/Scripts/BuildMechanic/BuildMechanic.cs
+++ CreateFallJam2018/Assets/Scripts/BuildMechanic/BuildMechanic.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class BuildMechanic : MonoBehaviour
 {
 	public GameObject wallPrefab;
+	public float yPosOffset = 1.5f;
 	
 	public void BuildWall()
 	{
 		//Checks for right mouse button click and if true it builds the prefab
 		if (Input.GetMouseButtonDown(1))
 		{
-			Instantiate(wallPrefab, transform.position + transform.forward * 2f, transform.rotation);
+			Vector3 position = new Vector3(transform.position.x, transform.position.y - yPosOffset, transform.position.z);
+			Instantiate(wallPrefab, position + transform.forward * 2f, transform.rotation);
 		}
 	}
 }
