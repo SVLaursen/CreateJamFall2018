e31bd8e6116a67fae982ee5c52bc0f7a8b3675a7
Night <tnight17@student.aau.dk>
2018-11-18 10:58:10 +0100

    Added BG Music

--- CreateFallJam2018/Assets/Scripts/Audio/AudioController.cs
+++ CreateFallJam2018/Assets/Scripts/Audio/AudioController.cs
@@ -9,6 +9,7 @@ public class AudioController : MonoBehaviour {
     public AudioSource hitSound;
     public AudioSource slingshotPull;
     public AudioSource catapultShoot;
+    public AudioSource BGMusic;
 
     // Use this for initialization
     void Start () {
@@ -16,6 +17,9 @@ public class AudioController : MonoBehaviour {
         hitSound = sounds[0];
         slingshotPull = sounds[1];
         catapultShoot = sounds[2];
+        BGMusic = sounds[3];
+
+        playBGMusic();
     }
 	
 	// Update is called once per frame
@@ -42,5 +46,15 @@ public class AudioController : MonoBehaviour {
     public void stopCatapultPull()
     {
         slingshotPull.Stop();
+    }
+
+    public void playBGMusic()
+    {
+        BGMusic.Play();
+    }
+
+    public void stopBGMusic()
+    {
+        BGMusic.Stop();
     }
 }
